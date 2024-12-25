b0e80ed8a19bc418bb8296f18c518547519c11a3
Hadeel-aldawsari <hadeel.amg99@gmail.com>
2024-12-25 15:10:29 +0300

    CustomerController.java

--- bank_system/Controller/CustomerController.java
+++ bank_system/Controller/CustomerController.java
@@ -1,17 +1,15 @@
-package com.example.bank_system.Controller;
+package com.example.bank_system_full.Controller;
 
 
-import com.example.bank_system.ApiResponse.ApiResponse;
-import com.example.bank_system.DTO.IN.CustomerDTOIN;
-import com.example.bank_system.Model.Customer;
-import com.example.bank_system.Service.CustomerService;
+import com.example.bank_system_full.ApiResponse.ApiResponse;
+import com.example.bank_system_full.DTO.IN.CustomerDTOIN;
+import com.example.bank_system_full.Model.MyUser;
+import com.example.bank_system_full.Service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bank/customer")
@@ -25,4 +23,27 @@ public class CustomerController {
         return ResponseEntity.status(200).body(new ApiResponse("Customer registered successfully"));
 
     }
+
+    //admin get all customer
+    @GetMapping("/admin/get-all-customer")
+    public ResponseEntity getAllCustomers(@AuthenticationPrincipal MyUser myUser){
+        return ResponseEntity.status(200).body(customerService.getAllCustomer());
+    }
+
+    //customer updated his profile
+    @PutMapping("/update")
+    public ResponseEntity update(@AuthenticationPrincipal MyUser myUser,@RequestBody @Valid CustomerDTOIN customerDTOIN){
+        customerService.update(myUser.getId(),customerDTOIN);
+        return ResponseEntity.status(200).body(new ApiResponse("updated successfully"));
+    }
+
+
+    @DeleteMapping("/delete")
+    public ResponseEntity delete(@AuthenticationPrincipal MyUser myUser){
+        customerService.delete(myUser.getId());
+        return ResponseEntity.status(200).body(new ApiResponse("delete successfully"));
+
+    }
+
+
 }
