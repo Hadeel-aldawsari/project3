745c3d9adef38802ec9ba1c7310f4410223321a8
Hadeel-aldawsari <hadeel.amg99@gmail.com>
2024-12-25 15:11:56 +0300

    EmployeeController.java

--- bank_system/Controller/EmployeeController.java
+++ bank_system/Controller/EmployeeController.java
@@ -1,17 +1,14 @@
-package com.example.bank_system.Controller;
+package com.example.bank_system_full.Controller;
 
-import com.example.bank_system.ApiResponse.ApiResponse;
-import com.example.bank_system.DTO.IN.CustomerDTOIN;
-import com.example.bank_system.DTO.IN.EmployeeDTOIN;
-import com.example.bank_system.Service.CustomerService;
-import com.example.bank_system.Service.EmployeeService;
+import com.example.bank_system_full.ApiResponse.ApiResponse;
+import com.example.bank_system_full.DTO.IN.EmployeeDTOIN;
+import com.example.bank_system_full.Model.MyUser;
+import com.example.bank_system_full.Service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bank/employee")
@@ -23,6 +20,26 @@ public class EmployeeController {
     public ResponseEntity EmployeeRegister(@RequestBody @Valid EmployeeDTOIN employeeDTOIN){
         employeeService.EmployeeRegister(employeeDTOIN);
         return ResponseEntity.status(200).body(new ApiResponse("Employee registered successfully"));
+    }
+
+    //admin get all customer
+    @GetMapping("/admin/get-all-employee")
+    public ResponseEntity getAllEmployee(@AuthenticationPrincipal MyUser myUser){
+        return ResponseEntity.status(200).body(employeeService.getAllEmployee());
+    }
+
+    //employee updated his profile
+    @PutMapping("/update")
+    public ResponseEntity update(@AuthenticationPrincipal MyUser myUser,@RequestBody @Valid EmployeeDTOIN employeeDTOIN){
+        employeeService.update(myUser.getId(),employeeDTOIN);
+        return ResponseEntity.status(200).body(new ApiResponse("updated successfully"));
+    }
+
+
+    @DeleteMapping("/delete")
+    public ResponseEntity delete(@AuthenticationPrincipal MyUser myUser){
+        employeeService.delete(myUser.getId());
+        return ResponseEntity.status(200).body(new ApiResponse("delete successfully"));
 
     }
 }
