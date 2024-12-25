f25844d3b129ea2344cf1089de4ca641fff1c3d2
Hadeel-aldawsari <hadeel.amg99@gmail.com>
2024-12-25 15:16:59 +0300

    EmployeeService.java

--- bank_system/Service/EmployeeService.java
+++ bank_system/Service/EmployeeService.java
@@ -1,16 +1,18 @@
-package com.example.bank_system.Service;
+package com.example.bank_system_full.Service;
 
-import com.example.bank_system.DTO.IN.CustomerDTOIN;
-import com.example.bank_system.DTO.IN.EmployeeDTOIN;
-import com.example.bank_system.Model.Customer;
-import com.example.bank_system.Model.Employee;
-import com.example.bank_system.Model.MyUser;
-import com.example.bank_system.Repository.CustomerRepository;
-import com.example.bank_system.Repository.EmployeeRepository;
-import com.example.bank_system.Repository.MyUserRepository;
+import com.example.bank_system_full.ApiResponse.ApiException;
+import com.example.bank_system_full.DTO.IN.EmployeeDTOIN;
+import com.example.bank_system_full.DTO.OUT.EmployeeDTOOUT;
+import com.example.bank_system_full.Model.Employee;
+import com.example.bank_system_full.Model.MyUser;
+import com.example.bank_system_full.Repository.EmployeeRepository;
+import com.example.bank_system_full.Repository.MyUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,4 +34,42 @@ public class EmployeeService {
         employeeRepository.save(employee);
 
     }
+
+    //admin get all Emp
+    public List<EmployeeDTOOUT> getAllEmployee(){
+        List<Employee> employees=employeeRepository.findAll();
+        List<EmployeeDTOOUT> dtos=new ArrayList<>();
+
+        for(Employee e:employees){
+            EmployeeDTOOUT out=new EmployeeDTOOUT(e.getUser().getUsername(),e.getUser().getName(),e.getUser().getEmail(),e.getPosition(),e.getSalary());
+            dtos.add(out);
+        }
+        return dtos;
+    }
+
+
+    //Emp update his data
+    public void update(Integer userid, EmployeeDTOIN employeeDTOIN){
+        Employee oldEmployee=employeeRepository.findEmployeeById(userid);
+        if(oldEmployee==null)throw new ApiException("employee not found");
+
+        String hashPass=new BCryptPasswordEncoder().encode(employeeDTOIN.getPassword());
+
+        oldEmployee.getUser().setName(employeeDTOIN.getName());
+        oldEmployee.getUser().setEmail(employeeDTOIN.getEmail());
+        oldEmployee.getUser().setPassword(hashPass);
+        oldEmployee.getUser().setUsername(employeeDTOIN.getUsername());
+        oldEmployee.setPosition(employeeDTOIN.getPosition());
+        oldEmployee.setSalary(employeeDTOIN.getSalary());
+        employeeRepository.save(oldEmployee);
+    }
+
+    //Emp
+    public  void delete(Integer emid){
+        MyUser myUser=myUserRepository.findMyUserById(emid);
+        if(myUser==null)throw new ApiException("no employee found by this id");
+
+        myUserRepository.delete(myUser);
+    }
+
 }
